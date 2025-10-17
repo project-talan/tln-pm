77f1d0abefece49eb00f7969d49591bd59dffd9a
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2025-10-17 16:37:17 +0300

    projects2 endpoint

--- src/server.js
+++ src/server.js
@@ -55,9 +55,300 @@ class Server {
     ea.get('/api/info', (req, res) => {
       res.send(this.makeResponce({version, scmUser: app.scmUser}));
     })
+    // PROJECTS
     ea.get('/api/projects', async(req, res) => {
       res.send(this.makeResponce( await app.describe({ what: { component: true } })));
     })
+    /*
+    ea.get('/api/projects2', async(req, res) => {
+      res.send(this.makeResponce( await app.describe({ what: { component: true } })));
+      const data = {
+        "projects": [
+          {
+            "id": "tln-pm",
+            "name": "Talan PM",
+            "description": "Project Management as Code",
+            "path": "",
+            "lastCommit": "2025-09-17 18:20:33 +0300",
+            "lastUpdateTime": "24d",
+            "team": [
+              {
+                "id": "vlad.k",
+                "name": "Vladyslav Kurmaz",
+                "involvements": [
+                  {
+                    "project": "tln-pm",
+                    "email": "vladislav.kurmaz@gmail.com"
+                  },
+                  {
+                    "project": "tln-cli",
+                    "email": "vladislav.kurmaz@gmail.com"
+                  }
+                ],
+                "stats": {
+                  "todo": 36,
+                  "dev": 1,
+                  "blocked": 0,
+                  "done": 68,
+                  "total": 105
+                }
+              }
+            ],
+                "timeline": [
+                    {
+                        "id": "0.24.0",
+                        "uid": "tln-pm@0.24.0",
+                        "date": "2025-11-05T16:00:00.000Z",
+                        "active": true,
+                        "current": false,
+                        "durationToRelease": 2151881670,
+                        "durationToReleaseHR": "24d"
+                    },
+                    {
+                        "id": "0.23.0",
+                        "uid": "tln-pm@0.23.0",
+                        "date": "2025-10-18T16:00:00.000Z",
+                        "active": true,
+                        "current": true,
+                        "durationToRelease": 596681670,
+                        "durationToReleaseHR": "6d"
+                    },
+                    {
+                        "id": "0.22.0",
+                        "uid": "tln-pm-0.22.0",
+                        "date": "2025-09-17T18:00:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.21.0",
+                        "uid": "tln-pm-0.21.0",
+                        "date": "2025-08-04T05:00:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.20.0",
+                        "uid": "tln-pm-0.20.0",
+                        "date": "2025-06-27T08:00:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.19.0",
+                        "uid": "tln-pm-0.19.0",
+                        "date": "2025-04-26T21:00:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.18.0",
+                        "uid": "tln-pm-0.18.0",
+                        "date": "2025-03-30T12:00:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.17.0",
+                        "uid": "tln-pm-0.17.0",
+                        "date": "2025-03-16T12:40:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.16.0",
+                        "uid": "tln-pm-0.16.0",
+                        "date": "2025-03-01T21:00:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.15.0",
+                        "uid": "tln-pm-0.15.0",
+                        "date": "2025-02-19T10:00:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.14.0",
+                        "uid": "tln-pm-0.14.0",
+                        "date": "2025-01-10T18:00:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.13.0",
+                        "uid": "tln-pm-0.13.0",
+                        "date": "2024-12-30T14:00:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.12.0",
+                        "uid": "tln-pm-0.12.0",
+                        "date": "2024-12-21T19:00:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.11.0",
+                        "uid": "tln-pm-0.11.0",
+                        "date": "2024-12-09T08:00:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.10.0",
+                        "uid": "tln-pm-0.10.0",
+                        "date": "2024-11-17T21:40:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.9.0",
+                        "uid": "tln-pm-0.9.0",
+                        "date": "2024-11-10T21:00:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.8.0",
+                        "uid": "tln-pm-0.8.0",
+                        "date": "2024-11-03T12:00:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.7.0",
+                        "uid": "tln-pm-0.7.0",
+                        "date": "2024-10-28T21:55:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.6.0",
+                        "uid": "tln-pm-0.6.0",
+                        "date": "2024-10-22T15:30:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.5.0",
+                        "uid": "tln-pm-0.5.0",
+                        "date": "2024-09-23T15:30:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.4.0",
+                        "uid": "tln-pm-0.4.0",
+                        "date": "2024-09-15T15:30:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.3.0",
+                        "uid": "tln-pm-0.3.0",
+                        "date": "2024-09-12T15:30:00.000Z",
+                        "active": false,
+                        "current": false
+                    },
+                    {
+                        "id": "0.2.0",
+                        "uid": "tln-pm-0.2.0",
+                        "date": "2024-09-04T15:30:00.000Z",
+                        "active": false,
+                        "current": false
+                    }
+                ],
+                "assessment": {
+                    "nfr": [
+                        {
+                            "id": "io.umlhub.nfr.recoverability",
+                            "name": "Recoverability",
+                            "value": 0.88
+                        },
+                        {
+                            "id": "io.umlhub.nfr.verifiability",
+                            "name": "Verifiability",
+                            "value": 0.16
+                        },
+                        {
+                            "id": "io.umlhub.nfr.functionality",
+                            "name": "Functionality",
+                            "value": 0.54
+                        }
+                    ]
+                },
+                "summary": {
+                    "release": {
+                        "id": "0.23.0",
+                        "uid": "tln-pm-0.23.0",
+                        "date": "2025-10-18T16:00:00.000Z",
+                        "active": true,
+                        "current": true,
+                        "durationToRelease": 596681670,
+                        "durationToReleaseHR": "6d",
+                        "features": 1,
+                        "improvements": 1
+                    },
+                    "team": {
+                        "size": 0,
+                        "total": 1,
+                        "utilization": [
+                            0.6,
+                            0.82,
+                            0.08,
+                            0.25,
+                            0,
+                            0.95,
+                            0.63,
+                            0.47,
+                            0.63,
+                            0.88
+                        ]
+                    },
+                    "tasks": {
+                        "todo": 36,
+                        "dev": 1,
+                        "blocked": 0,
+                        "done": 68
+                    }
+                }
+            }
+        ],
+        "team": [
+            {
+                "id": "vlad.k",
+                "name": "Vladyslav Kurmaz",
+                "bandwidth": [
+                    {
+                        "project": "tln-pm",
+                        "email": "vladislav.kurmaz@gmail.com"
+                    }
+                ],
+                "status": {
+                    "todo": 36,
+                    "dev": 1,
+                    "blocked": 0,
+                    "done": 68,
+                    "total": 105
+                }
+            }
+        ],
+        "timeline": [],
+        "stats": {
+          "todo": 36,
+          "dev": 1,
+          "blocked": 0,
+          "done": 68,
+          "total": 105
+        }
+    }
+}
+
+    })
+*/
     ea.get(['/api/tasks', '/api/tasks/:component*'], async(req, res) => {
       // console.log('req.params:', req.params);
       // console.log('req.query:', req.query);
@@ -94,7 +385,6 @@ class Server {
     ea.get('/api/assessments', async(req, res) => {
       res.send(this.makeResponce( await app.describe({ what: { assessments: true } })));
     })
-    //
     // SPA
     ea.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, '..', 'app', 'dist', 'index.html'));
